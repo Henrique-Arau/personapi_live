3e0567e997820d1d2bc04205ea9098d0ebc9a4b9
Henrique-Arau <penielh43@gmail.com>
2021-11-17 00:55:18 -0300

    Modelando o dominio da aplicação

---
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personapi.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME( description: "Home"),
+    MOBILE( description: "Mobile"),
+    COMMERCIAL( description: "Commercial");
+
+    private final String description;
+}
