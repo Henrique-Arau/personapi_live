a00d7c9ab73b3b61084c127305337a2106a27df1
Henrique-Arau <penielh43@gmail.com>
2021-11-19 00:32:37 -0300

    Developer get person by ID

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,12 +42,10 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Optional<Person> optionalPerson = personRepository.findById(id);
-        if(optionalPerson.isEmpty()) {
-            throw new PersonNotFoundException(id);
-        }
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
 
 
-        return personMapper.toDTO(optionalPerson.get());
+        return personMapper.toDTO(person);
     }
 }
